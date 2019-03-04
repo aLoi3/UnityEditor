07b54937319480455132a5d5d8bbc322aaf13071
Tomas <mazurkevic.tomas@gmail.com>
2019-03-04 16:48:56 +0000

    * Started making custom unity editor for AI component assignment

---
+++ DecisionMaker/Assets/Scripts/DecisionMakerEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CustomEditor( typeof( DecisionMaker ) )]
+public class DecisionMakerEditor : Editor {
+    // Have to play to choose enumerators
+
+    DecisionMaker DM;
+
+    public void OnEnable()
+    {
+        DM = (DecisionMaker)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DM.enumerator = EditorGUILayout.EnumPopup(DM.enumerator);
+    }
+}
