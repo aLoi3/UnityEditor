030b711479c91fdee352869e8a59eb859e8e9487
Tomas <mazurkevic.tomas@gmail.com>
2019-03-12 15:23:21 +0000

    Got it working finally, about half done

--- DecisionMaker/Assets/Scripts/Navigation/Nav1.cs
+++ DecisionMaker/Assets/Scripts/Navigation/Nav1.cs
@@ -1,23 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Nav1 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
+    private int test;
+    private Vector3 wanderTarget;
+    private float wanderRange = 3.0f;
+    private NavMeshHit navHit;
+    private float delta = 0.0f;
+    private NavMeshAgent myNavMeshAgent;
+    private Rigidbody rigidbody;
 
-    void PrintStr()
+    public Nav1(NavMeshAgent agent, Rigidbody rb)
     {
-        print("Navigating...");
+        test = 10;
+        myNavMeshAgent = agent;
+        rigidbody = rb;
+    }
+
+    public void PrintStr()
+    {
+        Debug.Log(test);
+    }
+
+    public int ScoreEvaluator()
+    {
+        return 80;
+    }
+
+    public void BehaviourExecute()
+    {
+        Debug.Log("I'm navigating...");
+        delta += Time.deltaTime;
+
+        // Move to another point every 3 seconds
+        if (delta >= 3.0f)
+        {
+            /* Next if statements are checks if the enemy has reached the assigned position */
+            // Checks if character is going to a position
+            if (!myNavMeshAgent.pathPending)
+            {
+                // Checks how far or close is the position
+                if (myNavMeshAgent.remainingDistance <= myNavMeshAgent.stoppingDistance)
+                {
+                    // Checks whether it has path and the movement velocity to after assign a new position
+                    if (!myNavMeshAgent.hasPath || myNavMeshAgent.velocity.sqrMagnitude == 0.0f)
+                    {
+                        // Find a random point to go to
+                        if (RandomWanderTarget(rigidbody.position, wanderRange, out wanderTarget))
+                        {
+                            // Debug drawing
+                            Debug.DrawRay(wanderTarget, Vector3.up, Color.blue, 1.0f);
+
+                            // Move to the assigned position
+                            myNavMeshAgent.SetDestination(wanderTarget);
+                        }
+                    }
+                }
+            }
+
+            delta = 0.0f;
+        }
+
+        
+    }
+
+    bool RandomWanderTarget(Vector3 centre, float range, out Vector3 result)
+    {
+        // Sets invisible sphere for enemy to decide where to go
+        Vector3 randomPoint = centre + Random.insideUnitSphere * wanderRange;
+        if (NavMesh.SamplePosition(randomPoint, out navHit, 1.0f, NavMesh.AllAreas))
+        {
+            // Sends out position for enemy to go to
+            result = navHit.position;
+            return true;
+        }
+        else
+        {
+            result = centre;
+            return false;
+        }
     }
 }
--- DecisionMaker/Assets/Scripts/ScriptData.cs
+++ DecisionMaker/Assets/Scripts/ScriptData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.AI;
 using System;
 using System.Reflection;
 
@@ -9,22 +10,96 @@ public class ScriptData : MonoBehaviour
 {
     public List<Scripts> data = new List<Scripts>();
     public GameObject holder;
-    
+    public List<string> classNames;
+    public List<Type> types;
+
+    private NavMeshAgent myNavMeshAgent;
+    private Rigidbody rigidbody;
+    private int highestScore = 0;
+    private int index;
     private MonoBehaviour[] myScripts;
+    private object classObject;
 
     private void Start()
     {
-        for (int i = 0; i < data.Count; i++)
+        // Create a list of class names
+        classNames = new List<string>();
+        types = new List<Type>();
+
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+        rigidbody = GetComponent<Rigidbody>();
+        classObject = new object[] { };
+
+        for (int i=0; i<data.Count; i++)
         {
-            print(data[i].Script.name);
+            // Get all class names into a list
+            classNames.Add(data[i].Script.name);
+            print(classNames[i]);
+
+            // Get the class
+            types.Add(Type.GetType(classNames[i]));
         }
 
+        //for (int i = 0; i < data.Count; i++)
+        //{
+        //    // Get all class names into a list
+        //    classNames.Add(data[i].Script.name);
+        //    print(classNames[i]);
 
+        //    // Also works
+        //    // Get the class
+        //    types.Add(Type.GetType(classNames[i]));
+
+        //    // Get the constructor and invoke it
+        //    ConstructorInfo constructor = types[i].GetConstructor(Type.EmptyTypes);
+        //    classObject = constructor.Invoke(new object[] { myNavMeshAgent, rigidbody });
+
+        //    // Get the method and invoke it
+        //    MethodInfo score = types[i].GetMethod("ScoreEvaluator");
+        //    object test = score.Invoke(classObject, new object[] { });
+
+        //    if ((int)test > highestScore)
+        //    {
+        //        index = i;
+        //        highestScore = (int)test;
+        //    }
+
+        //    Debug.Log(highestScore);
+        //}
+        //MethodInfo behaviour = types[index].GetMethod("BehaviourExecute");
+        //object behaviourMethod = behaviour.Invoke(classObject, new object[] { });
     }
 
-    private void Update()
+    public void Update()
     {
-        Type t = data[0].Script.GetType();
-        //t.GetMethod("PrintStr").Invoke(new object[] { });
+        for (int i = 0; i < data.Count; i++)
+        {
+            // Get the constructor and invoke it
+            ConstructorInfo constructor = types[i].GetConstructor(Type.EmptyTypes);
+            classObject = constructor.Invoke(this, new object[] { myNavMeshAgent, rigidbody });
+
+            // Get the method and invoke it
+            MethodInfo score = types[i].GetMethod("ScoreEvaluator");
+            object test = score.Invoke(classObject, new object[] { });
+
+            if ((int)test > highestScore)
+            {
+                index = i;
+                highestScore = (int)test;
+            }
+
+            Debug.Log(highestScore);
+        }
+        MethodInfo behaviour = types[index].GetMethod("BehaviourExecute");
+        object behaviourMethod = behaviour.Invoke(classObject, new object[] { });
+    }
+
+    public void GetHighscore(int score, int i)
+    {
+        if(score > highestScore)
+        {
+            highestScore = score;
+            index = i;
+        }
     }
 }
