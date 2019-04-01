7a66049785f354cec64fdccb7a77396e8affaffc
Tomas <mazurkevic.tomas@gmail.com>
2019-04-01 15:50:03 +0100

    * Made it working , but only with one script attached to the list To:Do - Make it work with multiple scripts

--- DecisionMaker/Assets/Scripts/ScriptData.cs
+++ DecisionMaker/Assets/Scripts/ScriptData.cs
@@ -13,12 +13,14 @@ public class ScriptData : MonoBehaviour
     public List<string> classNames;
     public List<Type> types;
 
-    //private NavMeshAgent myNavMeshAgent;
-    //private Rigidbody rigidbody;
+    private NavMeshAgent myNavMeshAgent;
+    private Rigidbody rigidbody;
     private int highestScore = 0;
     private int index;
     private MonoBehaviour[] myScripts;
     private object classObject;
+    private object scoreObject;
+    private object behaviourObject;
 
     private void Start()
     {
@@ -26,9 +28,12 @@ public class ScriptData : MonoBehaviour
         classNames = new List<string>();
         types = new List<Type>();
 
-        //myNavMeshAgent = GetComponent<NavMeshAgent>();
-        //rigidbody = GetComponent<Rigidbody>();
-        classObject = new object[] { };
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+        rigidbody = GetComponent<Rigidbody>();
+
+        //classObject = new object[] { };
+        //scoreObject = new object[] { };
+        //behaviourObject = new object[] { };
 
         //for (int i=0; i<data.Count; i++)
         //{
@@ -51,29 +56,20 @@ public class ScriptData : MonoBehaviour
             classNames.Add(data[i].Script.name);
             print(classNames[i]);
 
-            // Also works
             // Get the class
             types.Add(Type.GetType(classNames[i]));
 
-            // Get the constructor and invoke it
-            ConstructorInfo constructor = types[i].GetConstructor(Type.EmptyTypes);
-            classObject = constructor.Invoke(new object[] { });
-
-            // Get the method and invoke it
-            MethodInfo score = types[i].GetMethod("ScoreEvaluator");
-            object test = score.Invoke(classObject, new object[] { });
-
-            if ((int)test > highestScore)
-            {
-                index = i;
-                highestScore = (int)test;
-            }
-
-            Debug.Log(test);
+            //ConstructorInfo constructor = types[i].GetConstructor(new[] { typeof(NavMeshAgent), typeof(Rigidbody) });
+            //classObject = constructor.Invoke(classObject, new object[] { myNavMeshAgent, rigidbody });
         }
-        MethodInfo behaviour = types[index].GetMethod("BehaviourExecute");
-        object behaviourMethod = behaviour.Invoke(classObject, new object[] { });
-        Debug.Log(highestScore);
+
+        // Get the constructor and invoke it
+        foreach (Type type in types)
+        {
+            ConstructorInfo constructor = type.GetConstructor(new[] { typeof(NavMeshAgent), typeof(Rigidbody) });
+            classObject = constructor.Invoke(classObject, new object[] { myNavMeshAgent, rigidbody });
+        }
+
     }
 
     public void Update()
@@ -90,11 +86,28 @@ public class ScriptData : MonoBehaviour
         //}
         //MethodInfo behaviour = types[index].GetMethod("BehaviourExecute");
         //object behaviourMethod = behaviour.Invoke(classObject, new object[] { });
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            // Get the method and invoke it
+            MethodInfo score = types[i].GetMethod("ScoreEvaluator");
+            scoreObject = score.Invoke(classObject, new object[] { });
+            GetHighscore((int)scoreObject, i);
+
+            Debug.Log(highestScore);
+        }
+
+        MethodInfo behaviour = types[index].GetMethod("BehaviourExecute", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+        behaviourObject = behaviour.Invoke(classObject, new object[] { });
     }
 
     public void GetHighscore(int score, int i)
     {
-        if(score > highestScore)
+        if(score < highestScore)
+        {
+            return;
+        }
+        else
         {
             highestScore = score;
             index = i;
